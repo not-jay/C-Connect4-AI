9ed7d990f33482673f5f98fae62832fca8f074d3
not-jay <jaysonsona@tfwno.gf>
2014-10-18 13:14:15 +0800

    Removed winningTile

--- space.h
+++ space.h
@@ -5,10 +5,7 @@
 
 struct space {
 	int		owner;
-	bool	winningTile;
 	bool	(*isSpaceEmpty)(struct space *);
-	void	(*isWinningTile)(struct space *, bool);
-	bool	(*hasWinningTile)(struct space *);
 	void	(*put)(struct space *, int);
 	int		(*getOwner)(struct space *);
 	void	(*destroySpace)(struct space *);
@@ -18,8 +15,6 @@ typedef struct space *Space;
 
 Space	newSpace();
 bool	isSpaceEmpty(Space);
-void	isWinningTile(Space, bool);
-bool	hasWinningTile(Space);
 void	put(Space, int);
 int		getOwner(Space);
 void	destroySpace(Space);
@@ -27,11 +22,8 @@ void	destroySpace(Space);
 Space newSpace() {
 	Space space = (Space)calloc(1, sizeof(struct space));
 	space->owner = -1;
-	space->winningTile = FALSE;
 
 	space->isSpaceEmpty = isSpaceEmpty;
-	space->isWinningTile = isWinningTile;
-	space->hasWinningTile = hasWinningTile;
 	space->put = put;
 	space->getOwner = getOwner;
 	space->destroySpace = destroySpace;
@@ -41,14 +33,6 @@ Space newSpace() {
 
 bool isSpaceEmpty(Space s) {
 	return s->owner == -1;
-}
-
-void isWinningTile(Space s, bool isWinning) {
-	s->winningTile = isWinning;
-}
-
-bool hasWinningTile(Space s) {
-	return s->winningTile;
 }
 
 void put(Space s, int player) {
