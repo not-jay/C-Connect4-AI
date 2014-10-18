d7c582f85488b4ec363ecbdf84102e652cf98195
not-jay <jaysonsona@tfwno.gf>
2014-10-18 13:15:51 +0800

    Updated statusDraw Switched around the game loop

--- main.c
+++ main.c
@@ -1,15 +1,16 @@
 #include <curses.h>
 #include "board.h"
 #include "player.h"
+#include "ai.h"
 
 void boardDraw(Board);
-void statusDraw(WINDOW *, Player);
+void statusDraw(WINDOW *, Player, AI);
 
 int main() {
 	Board board = newBoard();
 	Player player = newPlayer(1, PLAYER, board),
-		   ai = newPlayer(2, AI, board),
 		   current;
+	AI ai = newAI(board);
 	WINDOW *status;
 	bool isRunning = TRUE;
 	int key;
@@ -28,21 +29,42 @@ int main() {
 	status = newwin(25, 20, 0, 0);
 	current = player;
 
+	boardDraw(board);
+	statusDraw(status, current, ai);
+	ungetch('a');	/* Skip first getch() */
+
 	while(isRunning) {
-		boardDraw(board);
-		statusDraw(status, current);
+		if((key = getch()) == KEY_F(1)) {
+			isRunning = FALSE;
+			break;
+		}
+		
 		if(current == player) {
 			if(player->doMove(player, key)) {
 				/* Pass turn to AI */
-				current = ai;
-				ungetch('a'); /* Skip the next keystroke and instantly go to the AI */
+				current = ai->player;
 			}
 		} else {
-			if(ai->doMove(ai, key)) {
+			ai->doAIMove(ai);
+			if(ai->player->doMove(ai->player, key)) {
 				/* Pass turn to AI */
 				current = player;
-				ungetch('a'); /* Skip the next keystroke and instantly go to the AI */
 			}
+		}
+
+		boardDraw(board);
+		current->doMove(current, 0);
+		statusDraw(status, current, ai);
+
+		if(board->isBoardFull(board)) {
+			wmove(status, 13, 1); wclrtoeol(status);
+			wattron(status, A_BOLD);
+			wprintw(status, "Draw Game!");
+			wattroff(status, A_BOLD);
+			wrefresh(status);
+
+			isRunning = FALSE;
+			getch();
 		}
 		if(board->checkForWin(board, current->getID(current))) {
 			int pid = current->getID(current);
@@ -52,14 +74,10 @@ int main() {
 			wprintw(status, "%s", (pid == 1)?"Player":"AI");
 			wattroff(status, A_BOLD | COLOR_PAIR(pid));
 			wprintw(status, " won!");
+			wrefresh(status);
 
 			isRunning = FALSE;
 			getch();
-		}
-
-		if((key = getch()) == KEY_F(1)) {
-			isRunning = FALSE;
-			break;
 		}
 	}
 
@@ -75,19 +93,19 @@ void boardDraw(Board b) {
 	b->draw(stdscr, b, 8, 33);
 }
 
-void statusDraw(WINDOW *win, Player p) {
+void statusDraw(WINDOW *win, Player p, AI ai) {
 	bool isPlayer = p->getID(p) == 1;
 
 	wattron(win, A_BOLD);
 	mvwprintw(win, 1, 1, "Versus AI: Hard");
-	mvwprintw(win, 5, 1, "Turn: ");
-	wclrtoeol(win);
+	mvwprintw(win, 5, 1, "Turn: "); wclrtoeol(win);
 	wattron(win, COLOR_PAIR(isPlayer?1:2));
 	wprintw(win, "%s", isPlayer?"Player":"AI");
 	wattroff(win, COLOR_PAIR(isPlayer?1:2));
-	mvwprintw(win, 9, 1, "AI Status:\n");
+	mvwprintw(win, 9, 1, "AI Status:");
 	wmove(win, 10, 1); wclrtoeol(win);
-	//AI status printing
+	wmove(win, 11, 1); wclrtoeol(win);
+	wprintw(win, "%s", !isPlayer?ai->getStatus(ai):"");
 	wattroff(win, A_BOLD);
 	wrefresh(win);
 }
