a1e67cdf968cc3bfba5dabc590981e9cf7ec5195
not-jay <jaysonsona@tfwno.gf>
2014-10-18 13:11:12 +0800

    Fixed bitboard generation Updated win checking algorithm Updated draw Fixed getSpace

--- board.h
+++ board.h
@@ -4,33 +4,32 @@
 #include <stdio.h>
 #include <curses.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include "space.h"
 
 struct board {
-	Space	 **spaces;
-	bool	(*isBoardEmpty)(struct board *);
-	Space	(*getSpace)(struct board *, int, int);
-	bool	(*isColumnFull)(struct board *, int);
-	void	(*dropPiece)(struct board *, int, int);
-	void	(*draw)(WINDOW *, struct board *, int, int);
-	bool	(*checkForWin)(struct board *, int);
-	void	(*destroyBoard)(struct board *);
-	unsigned long long int
-			(*getBitBoard)(struct board *, int);
+	Space		 **spaces;
+	bool		(*isBoardFull)(struct board *);
+	Space		(*getSpace)(struct board *, int, int);
+	bool		(*isColumnFull)(struct board *, int);
+	void		(*dropPiece)(struct board *, int, int);
+	void		(*draw)(WINDOW *, struct board *, int, int);
+	bool		(*checkForWin)(struct board *, int);
+	void		(*destroyBoard)(struct board *);
+	uint64_t	(*getBitBoard)(struct board *, int);
 };
 
 typedef struct board *Board;
 
-Board	newBoard();
-bool	isBoardEmpty(Board);
-Space	getSpace(Board, int, int);
-bool	isColumnFull(Board, int);
-void	dropPiece(Board, int, int);
-void	draw(WINDOW *, Board, int, int);
-bool	checkForWin(Board, int);
-void	destroyBoard(Board);
-unsigned long long int
-		getBitBoard(Board, int);
+Board		newBoard();
+bool		isBoardFull(Board);
+Space		getSpace(Board, int, int);
+bool		isColumnFull(Board, int);
+void		dropPiece(Board, int, int);
+void		draw(WINDOW *, Board, int, int);
+bool		checkForWin(Board, int);
+void		destroyBoard(Board);
+uint64_t	getBitBoard(Board, int);
 
 Board newBoard() {
 	Board board = (Board)calloc(1, sizeof(struct board));
@@ -44,6 +43,7 @@ Board newBoard() {
 		}
 	}
 
+	board->isBoardFull = isBoardFull;
 	board->getSpace = getSpace;
 	board->isColumnFull = isColumnFull;
 	board->dropPiece = dropPiece;
@@ -55,22 +55,18 @@ Board newBoard() {
 	return board;
 }
 
-bool isBoardEmpty(Board b) {
-	bool empty = TRUE;
-	Space current;
-	int x, y;
+bool isBoardFull(Board b) {
+	int x;
 
-	for(x = 5; x >= 0; x--) {
-		for(y = 6; y >= 0; y--) {
-			current = b->getSpace(b, x, y);
-			if(!current->isSpaceEmpty(current)) return FALSE;
-		}
+	for(x = 0; x < 7; x++) {
+		if(!isColumnFull(b, x)) return FALSE;
 	}
 
 	return TRUE;
 }
 
 Space getSpace(Board b, int x, int y) {
+	if(x < 0 || x > 5 || y < 0 || y > 6) return NULL;
 	return b->spaces[x][y];
 }
 
@@ -102,7 +98,7 @@ void dropPiece(Board b, int player, int col) {
 
 /* Here be dragons */
 void draw(WINDOW * win, Board b, int offsetY, int offsetX) {
-	int i, j, temp;
+	int i, j;
 	Space current;
 
 	wmove(win, offsetY, offsetX); clrtoeol();
@@ -114,11 +110,9 @@ void draw(WINDOW * win, Board b, int offsetY, int offsetX) {
 		wmove(win, offsetY+i+1, offsetX); clrtoeol(); addch(ACS_VLINE);
 		for(j = 0; j < 7; j++) {
 			current = b->getSpace(b, i, j);
-			if(current->hasWinningTile(current)) wattron(win, A_BLINK);
 			wattron(win, COLOR_PAIR(current->getOwner(current)+2));
 			wmove(win, offsetY+i+1, offsetX+(j*2)+1); clrtoeol(); addch(' ');
 			wattroff(win, COLOR_PAIR(current->getOwner(current)+2));
-			if(current->hasWinningTile(current)) wattroff(win, A_BLINK);
 			addch(ACS_VLINE);
 		}
 	}
@@ -131,28 +125,21 @@ void draw(WINDOW * win, Board b, int offsetY, int offsetX) {
 }
 
 /**
- *	Board checking algorithm by Christian Ammer 
- *	http://stackoverflow.com/a/7053051
- *
- *	Full code @ GitHub
+ *	Board checking algorithm by John Tromp
+ *	https://github.com/qu1j0t3/fhourstones/blob/master/Game.c
  *	
  */
 bool checkForWin(Board b, int player) {
-	unsigned long long int bitboard = b->getBitBoard(b, player);
-	bitboard &= bitboard >> 6;
-
-	if (bitboard & (bitboard >> 2 * 6))     // check \ diagonal
-        return TRUE;
-    bitboard &= bitboard >> 7;
-    if (bitboard & (bitboard >> 2 * 7))     // check horizontal
-        return TRUE;
-    bitboard &= bitboard >> 8;
-    if (bitboard & (bitboard >> 2 * 8))     // check / diagonal
-        return TRUE;
-    bitboard &= bitboard >> 1;
-    if (bitboard & (bitboard >> 2))         // check vertical
-        return TRUE;
-    return FALSE;
+	uint64_t bitboard = b->getBitBoard(b, player);
+	
+	uint64_t diag1 = bitboard & (bitboard >> 6);
+	uint64_t hori = bitboard & (bitboard >> 7);
+	uint64_t diag2 = bitboard & (bitboard >> 8);
+	uint64_t vert = bitboard & (bitboard >> 1);
+	return ((diag1 & (diag1 >> 12)) |
+			(hori & (hori >> 14)) |
+			(diag2 & (diag2 >> 16)) |
+			(vert & (vert >> 2))) != 0;
 }
 
 void destroyBoard(Board b) {
@@ -173,21 +160,23 @@ void destroyBoard(Board b) {
 	b->spaces = NULL;
 }
 
-unsigned long long int getBitBoard(Board b, int player) {
-	unsigned long long int bitboard = 0;
+uint64_t getBitBoard(Board b, int player) {
+	uint64_t bitboard = 0;
 	int x, y, count = 0;
 	Space current;
 
-	for(y = 0; x < 7; y++) {
-		for(x = 5; y >= 0; x--) {
+	for(y = 0; y < 7; y++) {
+		for(x = 5; x >= 0; x--) {
 			current = b->getSpace(b, x, y);
 			if(current->getOwner(current) == player) {
-				bitboard |= 1 >> count;
+				bitboard |= (uint64_t)1 << count;
 			}
 			count++;
 		}
 		count++;
 	}
+
+	return bitboard;
 }
 
 #endif
